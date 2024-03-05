95d970248b0edf28ffb5c0745fff86dd8060d97d
Vivek <sakariyavivek12@gmail.com>
2024-03-05 14:55:23 -0500

    fetched note using context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext"
 
 function Home() {
+    const context = useContext(noteContext)
+    const {notes,setNotes} = context
   return (
     <div>
       <div className="container my-3">
@@ -47,6 +50,9 @@ function Home() {
       </div>
       <div className="container">
         <h2>Your Notes</h2>
+        {notes.map((note)=>{
+            return note.title
+        })}
       </div>
     </div>
   );
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 const NoteState = (props) => {
+    const notesInitial = [
+        {
+          "_id": "65e2111f88a2fa72aaad4dfc",
+          "user": "65e0c14815d31d428d74b1f1",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "timestamp": "2024-03-01T17:32:15.197Z",
+          "__v": 0
+        },
+        {
+          "_id": "65e2111f88a2fa72aaad4dfe",
+          "user": "65e0c14815d31d428d74b1f1",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "timestamp": "2024-03-01T17:32:15.747Z",
+          "__v": 0
+        },
+        {
+          "_id": "65e2112088a2fa72aaad4e00",
+          "user": "65e0c14815d31d428d74b1f1",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "timestamp": "2024-03-01T17:32:16.531Z",
+          "__v": 0
+        },
+        {
+          "_id": "65e2112188a2fa72aaad4e02",
+          "user": "65e0c14815d31d428d74b1f1",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "timestamp": "2024-03-01T17:32:17.633Z",
+          "__v": 0
+        }
+      ]
+      const [notes,setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
