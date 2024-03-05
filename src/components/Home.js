26b5042e6ac5b2437f38eb51ac51e4961e0bfe2b
Vivek <sakariyavivek12@gmail.com>
2024-03-05 15:10:07 -0500

    segregated few components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext"
+import React from "react";
+import Notes from "./Notes";
 
 function Home() {
-    const context = useContext(noteContext)
-    const {notes,setNotes} = context
+    
   return (
     <div>
       <div className="container my-3">
@@ -48,12 +47,7 @@ function Home() {
           </button>
         </form>
       </div>
-      <div className="container">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-            return note.title
-        })}
-      </div>
+      <Notes/>
     </div>
   );
 }
